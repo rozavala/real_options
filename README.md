7b44c03e986ed9206a5d4298beac2b78c908bc17
Rodrigo Basoalto <rodrigo@basoalto.cl>
2025-09-25 13:44:17 -0300

    prediction api

---
+++ README.md
@@ -0,0 +1,26 @@
+# Real Options
+
+## Prediction API
+
+Para empezar necesitas `uv`, [homepage](https://github.com/astral-sh/uv), instálalo con `brew install uv` en mac, o como quieras en otras plataformas. (TODO: meterlo en un container de Docker pa no depender de ambiente local)
+
+Para echarlo a andar, `uv run prediction_api.py` y va a escuchar en el puerto 8000
+También se puede echar a andar con `uv run uvicon --port 12345 prediction_api:app` para el puerto 12345, y se puede agregar el flag `--reload` para hacer hot-reloading cuando cambias el código
+
+Ahi expone esta api:
+
+#### `POST /predictions`
+Encola un nuevo job de predicción, recibe un JSON con datos (se puede editar el modelo `PredictionJobCreateRequest` en `prediction_api.py` para modificar los campos), y retorna el estado del job pendiente, incluyendo su id
+
+```
+> curl --json '{"data":"date,price\n2025-01-01,100\n2025-01-02,101"}' localhost:8000/predictions
+{"id":"c32de7e5-35eb-44d1-a86c-41df0ca5b873","status":"pending","result":null,"error":null}
+```
+
+#### `GET /predictions/{id}`
+Obtiene el estado de un job de predicción con el ID dado.
+
+```
+> curl localhost:8000/predictions/c32de7e5-35eb-44d1-a86c-41df0ca5b873
+{"id":"a3ce44a7-ae79-4255-b5a1-0bbf188c23b1","status":"completed","result":{"predicted_price":100.0},"error":null}
+```
---
+++ prediction_api.py
@@ -0,0 +1,107 @@
+from enum import StrEnum
+import time
+import uuid
+from fastapi import FastAPI, HTTPException, status, BackgroundTasks, Request, Response
+from pydantic import BaseModel, model_validator
+
+
+class PredictionJobCreateRequest(BaseModel):
+    ### aqui agregamos lo q haya q recibir
+    data: str
+
+
+class PredictionResult(BaseModel):
+    predicted_price: float
+
+
+class PredictionJobStatus(StrEnum):
+    PENDING = "pending"
+    COMPLETED = "completed"
+    FAILED = "failed"
+
+
+class PredictionJob(BaseModel):
+    id: uuid.UUID
+    status: PredictionJobStatus
+    result: PredictionResult | None = None
+    error: str | None = None
+
+    @model_validator(mode="after")
+    def validate_result(self):
+        if self.status == PredictionJobStatus.PENDING:
+            if self.result is not None or self.error is not None:
+                raise ValueError("result and error must be None if status is PENDING")
+        elif self.status == PredictionJobStatus.COMPLETED:
+            if self.result is None:
+                raise ValueError("result must not be None if status is COMPLETED")
+            if self.error is not None:
+                raise ValueError("error must be None if status is COMPLETED")
+        elif self.status == PredictionJobStatus.FAILED:
+            if self.result is None:
+                raise ValueError("result must be None if status is FAILED")
+            if self.error is None:
+                raise ValueError("error must not be None if status is FAILED")
+        return self
+
+
+app = FastAPI()
+
+
+# aqui se guardan los resultados de las predicciones... ojo q estan en memoria nomas, si se reinicia el proceso se pierden, y nunca las borramos asiq se podrian acumular
+_predictions: dict[uuid.UUID, PredictionJob] = {}
+
+
+def compute_prediction(prediction_id: uuid.UUID, data: PredictionJobCreateRequest):
+    try:
+        ### Aqui hacemos la pega... usando data
+        time.sleep(30)  # simulando 30s
+        ### y usamos el resultado
+        prediction_result = PredictionResult(predicted_price=100.0)
+        ### y lo guardamos en el dict pa q despues se pueda consultar el resultado
+        _predictions[prediction_id] = PredictionJob(
+            id=prediction_id,
+            status=PredictionJobStatus.COMPLETED,
+            result=prediction_result,
+        )
+    except Exception as e:
+        _predictions[prediction_id] = PredictionJob(
+            id=prediction_id,
+            status=PredictionJobStatus.FAILED,
+            error=str(e),
+        )
+
+
+@app.post("/predictions", status_code=status.HTTP_202_ACCEPTED)
+def predict(
+    data: PredictionJobCreateRequest,
+    background_tasks: BackgroundTasks,
+    request: Request,
+    response: Response,
+) -> PredictionJob:
+    new_prediction_id = uuid.uuid4()
+    job = PredictionJob(
+        id=new_prediction_id,
+        status=PredictionJobStatus.PENDING,
+    )
+    _predictions[new_prediction_id] = job
+    background_tasks.add_task(compute_prediction, new_prediction_id, data)
+    response.status_code = status.HTTP_201_CREATED
+    response.headers["Location"] = str(
+        request.url_for("get_prediction", prediction_id=new_prediction_id)
+    )
+    return job
+
+
+@app.get("/predictions/{prediction_id}")
+def get_prediction(prediction_id: uuid.UUID) -> PredictionJob:
+    if prediction_id not in _predictions:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Prediction not found"
+        )
+    return _predictions[prediction_id]
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
