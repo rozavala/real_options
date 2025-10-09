5147497f6361cb54fd0d470532a4bfe247e7fe3b
rozavala <rozavala@gmail.com>
2025-10-09 09:12:31 -0700

    Update orchestrator.py

--- orchestrator.py
+++ orchestrator.py
@@ -122,9 +122,9 @@ async def main():
 
     # New schedule mapping run times (GMT) to functions
     schedule = {
-        time(15, 55): generate_and_queue_orders,
-        time(15, 56): start_monitoring,
-        time(15, 58): place_queued_orders,
+        time(16, 14): generate_and_queue_orders,
+        time(16, 17): start_monitoring,
+        time(16, 16): place_queued_orders,
         time(17, 0): close_all_open_positions,
         time(17, 30): cancel_and_stop_monitoring,
         time(18, 0): analyze_performance
