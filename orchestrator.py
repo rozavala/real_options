cc125d9d4cc359308abbece5bd443349f41c4f92
rozavala <rozavala@gmail.com>
2025-10-14 12:10:16 -0700

    Update orchestrator.py

--- orchestrator.py
+++ orchestrator.py
@@ -164,11 +164,11 @@ async def analyze_and_archive(config: dict):
 
 # New schedule mapping run times (GMT) to functions
 schedule = {
-    time(14, 25): generate_and_queue_orders,
-    time(14, 30): start_monitoring,
-    time(14, 27): place_queued_orders,
+    time(8, 45): generate_and_queue_orders,
+    time(8, 30): start_monitoring,
+    time(8, 55): place_queued_orders,
     time(17, 16): close_all_open_positions,
-    time(17, 20): cancel_and_stop_monitoring,
+    time(19, 12): cancel_and_stop_monitoring,
     time(18, 0): analyze_and_archive
 }
 
