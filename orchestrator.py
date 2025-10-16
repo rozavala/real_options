e9a6e37686db5d22fa54859c9eb013d5b7113954
rozavala <rozavala@gmail.com>
2025-10-16 15:30:14 -0700

    Update schedule times for monitoring and order functions

--- orchestrator.py
+++ orchestrator.py
@@ -162,9 +162,9 @@ async def analyze_and_archive(config: dict):
 
 # New schedule mapping run times (GMT) to functions
 schedule = {
-    time(21, 33): start_monitoring,
-    time(21, 34): generate_and_queue_orders,
-    time(21, 35): place_queued_orders,
+    time(22, 31): start_monitoring,
+    time(22, 32): generate_and_queue_orders,
+    time(22, 34): place_queued_orders,
     time(17, 25): close_all_open_positions,
     time(17, 29): cancel_and_stop_monitoring,
     time(18, 0): analyze_and_archive
